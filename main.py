b66526c0c192b385a1d446d46131fefce9e53fe5
Pugovkin Vyacheslav <pugovka3300@mail.ru>
2022-06-03 12:22:04 +0300

    123

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(userText)
+
+
+if __name__ == "__main__":
+    app.run()
